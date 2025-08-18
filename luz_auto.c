d357ff7aaba4f1ea05d54df3ff5fce07ca9e49b6
Caique <caiquedebritoo@gmail.com>
2025-08-18 20:45:35 -0300

    Refactor display logic and improve string formatting in luz_auto.c

--- luz_auto.c
+++ luz_auto.c
@@ -9,7 +9,7 @@
 #include "bh1750_light_sensor.h"
 #include "matriz.h"
 #include "i2c.h"
- 
+
 #define matriz_led 7
 #define endereco 0x3c
 
@@ -17,8 +17,11 @@ uint32_t luz = 0;
 char str_luz [16];
 ssd1306_t ssd;
 
+bool luz_alta = false;
+
 void display_core(void);
 void oledinit(void);
+uint8_t xcenter_pos(char* text);
 
 int main(){
     set_sys_clock_khz(48000, true);
@@ -31,7 +34,7 @@ int main(){
 
     while (true) {
         luz = bh1750_read_measurement(i2c_port0);
-        snprintf(str_luz, sizeof(str_luz), "Luz = %d", luz);
+        snprintf(str_luz, sizeof(str_luz), "Nivel = %d", luz);
         printf("Luz atual: %d\n", luz);
         (luz < 20) ? matriz(255, 255, 255) : matriz(0, 0, 0);
         sleep_ms(200);
@@ -42,7 +45,12 @@ void display_core(void){
     init_i2c1();
     oledinit();
     while(true){
-        ssd1306_draw_string (&ssd, str_luz, 0, 25);
+        ssd1306_draw_string(&ssd, "Luz", xcenter_pos("Luz"), 2);
+        ssd1306_draw_string(&ssd, "Emergencial", xcenter_pos("Emergencial"), 10);
+        ssd1306_hline(&ssd, 0, WIDTH-1,  20, true);
+        ssd1306_draw_string(&ssd, str_luz, xcenter_pos(str_luz), 28);
+        (luz_alta) ? ssd1306_draw_string(&ssd, "Alta", xcenter_pos("Alta"), 40) : ssd1306_draw_string(&ssd, "Baixa", xcenter_pos("Baixa"), 40);
+        ssd1306_draw_string(&ssd, "Modo manual:OFF", xcenter_pos("Modo manual:OFF"), 54);
         ssd1306_send_data(&ssd);
         sleep_ms(50);
     }
@@ -53,4 +61,8 @@ void oledinit(void){
     ssd1306_config(&ssd);
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
+}
+
+uint8_t xcenter_pos(char* text) {
+    return (WIDTH - 8 * strlen(text)) / 2; // Calcula a posição centralizada
 }
